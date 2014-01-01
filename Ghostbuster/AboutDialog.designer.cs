827f45f4c6c02462ecf3c244cef8c7f3cd0b3226
wvd_vegt_cp <SND\wvd_vegt_cp>
2014-01-01 11:03:11 -0800

    1) Updated donate button to english.

--- Ghostbuster/AboutDialog.designer.cs
+++ Ghostbuster/AboutDialog.designer.cs
@@ -43,7 +43,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::Ghostbuster.Properties.Resources.btn_donateCC_LG;
+            this.pictureBox1.Image = global::Ghostbuster.Properties.Resources.btn_donateCC_LG_EN;
             this.pictureBox1.Location = new System.Drawing.Point(12, 142);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(331, 100);
